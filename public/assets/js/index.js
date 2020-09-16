6b3540157e4f5794a919ef66ca6b3c257eaa4fed
Thomas Mullaney <tloring.mullaney@gmail.com>
2020-09-16 16:44:28 -0700

    changed location of service worker register

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,6 +1,15 @@
 let transactions = [];
 let myChart;
 
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("service-worker.js") 
+    .then(reg => console.log("Found your service worker!.", reg))
+    .catch(error => console.log("Service Worker registration failed:", error));
+  });
+};
+
 fetch("/api/transaction")
   .then(response => {
     return response.json();
