678362305db61e1c0b664cbaa1902c0fbe400bf0
Thomas Mullaney <tloring.mullaney@gmail.com>
2020-11-10 17:22:22 -0800

    caching correctly and offline is running

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,15 +3,11 @@ const FILES_TO_CACHE = [
   "/index.html",
   "/index.js",
   "/db.js",
-  "/style.css",
-  "/favicon.ico",
-  "manifest.webmanifest",
-  "/assets/images/icons/icon-192x192.png",
-  "/assets/images/icons/icon-512x512.png"
+  "/style.css"
 ];
 
-const CACHE_NAME = "static-cache-v2";
-const DATA_CACHE_NAME = "data-cache-v1";
+const CACHE_NAME = "my-site-cache-v2";
+const DATA_CACHE_NAME = "site-cache-v1";
 
 
 
